a30baf4b7c29d24617c1b8b1ebc267ce507a0416
Mitya Koval <dima.alex.koval@gmail.com>
2019-01-28 14:39:59 +0100

    http client

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,12 @@ export class HeroDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
+  
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+  
   goBack(): void {
     this.location.back();
   }
